ca4d5cad391bbd2870e585e26693dae838a88b35
Anila102 <anilaj557@gmail.com>
2022-09-19 17:06:58 +0500

    first

--- pages/Student/Quiz/[name].js
+++ pages/Student/Quiz/[name].js
@@ -2,27 +2,13 @@ import styles from "./name.module.css"
 import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/router"
 const Name = () => {
-    const [ms, setMs] = useState(1000)
-    const [second, setSecond] = useState(0)
+  const [ms, setMs] = useState(0)
+  const [second, setSecond] = useState(0)
+  const [minute, setMinute] = useState(0)
 
-    const router = useRouter()
-    const {name} = router.query
+  const router = useRouter()
+  const { name } = router.query
 
-useEffect(() => {
-
-if (ms>0) {
-    setMs(ms-1)
-   setSecond(second+1)
-}
-
- if (ms <=0) {
-    alert("times Up")
- }
-
-}, [ms])
-
-    
-    
 
 
 
@@ -30,13 +16,19 @@ if (ms>0) {
     <div>
 
 
-<div className={styles.Nav}>
-<div> <h1>  Quiz For {name} </h1> </div>
-<div> <h2>Timer {ms}  </h2> </div>
-<div> <h2>Timer {ms}  </h2> </div>
+      <div className={styles.Nav}>
+        <div> <h1>  Quiz For {name} </h1> </div>
+        {/* <div> <h2>miliSecond {ms}  </h2> </div>
+        <div> <h2>Seconds {second}  </h2> </div>
+        <div> <h2> Minute {second}  </h2> </div> */}
 
 
+
+<div className={styles.QuizContainer}>
+  <div> </div>
 </div>
+
+      </div>
 
 
 
