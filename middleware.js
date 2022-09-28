453cd1505bd55a1052a7ee8e6fee47f9312c7c5c
SHAHEERSHAHZADofficial <mohammedshaheershahzadrawda5@gmail.com>
2022-09-28 13:00:42 +0500

    hi

---
+++ middleware.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+export default function middleware(req){
+    let verify = req.cookies.get("authToken");
+    let url = req.url
+    
+    if(!verify && url==(`${req?.nextUrl?.protocol}//${req?.nextUrl?.host}/Student/Home` )){
+        return NextResponse.redirect(`${req?.nextUrl?.protocol}//${req?.nextUrl?.host}/Auth/Login`);
+    }
+
+    if (verify && url === `${req?.nextUrl?.protocol}//${req?.nextUrl?.host}/Auth/Login`) {
+      return NextResponse.redirect(`${req?.nextUrl?.protocol}//${req?.nextUrl?.host}/Student/Home`);
+    }
+
+console.log(`${req?.nextUrl?.protocol}//${req?.nextUrl?.host}/Student/Home`)
+}
+
